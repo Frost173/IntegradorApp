3a1bb4c60af3f2da35bb44702abbd2bfc32f6661
Jordan <jordantrejo45@gmail.com>
2025-05-09 20:49:55 -0500

    Second commit

--- app/src/main/java/com/cochera/miproyectointegrador/Login/LoginContract.java
+++ app/src/main/java/com/cochera/miproyectointegrador/Login/LoginContract.java
@@ -2,6 +2,8 @@ package com.cochera.miproyectointegrador.Login;
 
 public interface LoginContract {
     interface View {
+
+        //prueba
         //void showLoginSuccess(int clienteId, String nombre, String correo);
         //void showLoginError(String message);
 
